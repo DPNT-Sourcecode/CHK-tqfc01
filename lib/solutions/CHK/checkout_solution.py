a5840d6dffee0cfb0af7ced4cc34cea48c1245e9
sbx_user1051 <sbx_user1051@169.254.32.109>
2024-04-30 16:58:36 +0000

    Tue Apr 30 16:58:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,16 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+import re
+
+ALLOWED_SKUS = 'ABCD'
+
 def checkout(skus):
-    raise NotImplementedError()
+    if isinstance(skus, str) and bool(re.match(f'^[{ALLOWED_SKUS}]+$', skus)):
+        unique_skus = list(set(skus))
+        for sku in unique_skus:
+            count = skus.count(sku)
+    else:
+        return -1
+
