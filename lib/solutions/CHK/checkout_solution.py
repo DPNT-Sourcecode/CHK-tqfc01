f9888711cebb14e50c54ce39cad583f38f70ce88
sbx_user1051 <sbx_user1051@169.254.32.109>
2024-04-30 22:14:06 +0000

    Tue Apr 30 22:14:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,40 +6,40 @@
 import re
 # import math
 
-ALLOWED_SKUS = 'ABCDE'
+ALLOWED_SKUS = "ABCDE"
 
 PRICE_TABLE = {
-    'A': 50,
-    'B': 30,
-    'C': 20,
-    'D': 15,
-    'E': 40
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40
 }
 
 OFFERS_TABLE = [
-    {'offer': 'AAAAA', 'price': 200},
-    {'offer': 'AAA', 'price': 130},
-    {'offer': 'BEE', 'price': 80},
-    {'offer': 'BB', 'price': 45}
+    {"offer": "AAAAA", "price": 200},
+    {"offer": "AAA", "price": 130},
+    {"offer": "BEE", "price": 80},
+    {"offer": "BB", "price": 45}
 ]
 
 def find_and_remove(needles, haystack):
     if any(n not in haystack for n in needles):
         return False, haystack
     for n in needles:
-        haystack = haystack.replace(n, '', 1)
+        haystack = haystack.replace(n, "", 1)
     return True, haystack
 
 def checkout(skus):
-    if isinstance(skus, str) and bool(re.match(f'^[{ALLOWED_SKUS}]*$', skus)):
-        skus = ''.join(sorted(skus))
+    if isinstance(skus, str) and bool(re.match(f"^[{ALLOWED_SKUS}]*$", skus)):
+        skus = "".join(sorted(skus))
         total_sum = 0
 
         for row in OFFERS_TABLE:
             while True:
-                offer_found = find_and_remove(row.get('offer'), skus)
+                offer_found, skus = find_and_remove(row.get("offer", ""), skus)
                 if offer_found:
-                    total_sum += row.get('price')
+                    total_sum += row.get("price", 0)
                 else:
                     break
 
@@ -49,3 +49,4 @@ def checkout(skus):
         return total_sum
     else:
         return -1
+
