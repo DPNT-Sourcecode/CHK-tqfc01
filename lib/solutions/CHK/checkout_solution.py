35641d9a719d60becf27abbb9865acb7a89e301e
sbx_user1051 <sbx_user1051@169.254.32.109>
2024-04-30 22:10:49 +0000

    Tue Apr 30 22:10:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,48 +4,63 @@
 # skus = unicode string
 
 import re
-import math
+# import math
 
 ALLOWED_SKUS = 'ABCDE'
 
-PRICE_TABLE = [
-    {'sku': 'A', 'price': 50},
-    {'sku': 'B', 'price': 30},
-    {'sku': 'C', 'price': 20},
-    {'sku': 'D', 'price': 15},
-    {'sku': 'E', 'price': 40}
+PRICE_TABLE = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40
+}
+
+OFFERS_TABLE = [
+    {'offer': 'AAAAA', 'price': 200},
+    {'offer': 'AAA', 'price': 130},
+    {'offer': 'BEE', 'price': 80},
+    {'offer': 'BB', 'price': 45}
 ]
 
-OFFER_TABLE = {
-    'A': [
-        {'offer': 5, 'offer_price': 200},
-        {'offer': 3, 'offer_price': 130}
-    ],
-    'B': [
-        {'offer': 2, 'offer_price': 45}
-    ],
-    'E': [
-        {'offer': 3, 'offer_price': 80}
-    ]
-}
+def find_and_remove(needles, haystack):
+    if any(n not in haystack for n in needles):
+        return False
+    for n in needles:
+        haystack.replace(n, '', 1)
+    return True
 
 def checkout(skus):
     if isinstance(skus, str) and bool(re.match(f'^[{ALLOWED_SKUS}]*$', skus)):
+        skus = ''.join(sorted(skus))
         total_sum = 0
-        for price_row in PRICE_TABLE:
-            sku = price_row.get('sku')
-            count = skus.count(sku)
+        for row in OFFERS_TABLE:
+            while True:
+                index = skus.find(row.get('offer'))
+                if index > -1:
+                    skus = skus[:index] + skus[index + 1:]
+                    total_sum += row.get('price')
+                else:
+                    break
+        for sku in skus:
+            total_sum += PRICE_TABLE.get(sku, 0)
 
-            if count:
-                offers = OFFER_TABLE.get(sku, [])
-                for offer_row in offers:
-                    offer = offer_row.get('offer')
-                    if count >= offer:
-                        offer_count = math.floor(count / offer)
-                        count = count % offer
-                        total_sum += offer_count * offer_row.get('offer_price', 0)
+        # total_sum = 0
+        # for price_row in PRICE_TABLE:
+        #     sku = price_row.get('sku')
+        #     count = skus.count(sku)
+
+        #     if count:
+        #         offers = OFFER_TABLE.get(sku, [])
+        #         for offer_row in offers:
+        #             offer = offer_row.get('offer')
+        #             if count >= offer:
+        #                 offer_count = math.floor(count / offer)
+        #                 count = count % offer
+        #                 total_sum += offer_count * offer_row.get('offer_price', 0)
                 
-                total_sum += count * price_row.get('price', 0)
+        #         total_sum += count * price_row.get('price', 0)
         return total_sum
     else:
         return -1
+
