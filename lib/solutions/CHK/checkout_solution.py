1217d291f2a385470955b4d000834b95ef89af12
sbx_user1051 <sbx_user1051@169.254.32.109>
2024-04-30 17:07:36 +0000

    Tue Apr 30 17:07:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,13 +4,27 @@
 # skus = unicode string
 
 import re
+import math
 
-ALLOWED_SKUS = 'ABCD'
+ALLOWED_SKUS = [
+    {'sku': 'A', 'price': 50, 'offer': 3, 'offer_price': 130},
+    {'sku': 'B', 'price': 30, 'offer': 2, 'offer_price': 45},
+    {'sku': 'C', 'price': 20},
+    {'sku': 'D', 'price': 15}
+]
 
 def checkout(skus):
     if isinstance(skus, str) and bool(re.match(f'^[{ALLOWED_SKUS}]+$', skus)):
-        unique_skus = list(set(skus))
-        for sku in unique_skus:
-            count = skus.count(sku)
+        for i in list(ALLOWED_SKUS):
+            count = skus.count(i)
+            if i == 'A' and count >= 3:
+                offer_count = math.floor(count / 3)
+                remainder = count % 3
+            if i == 'A' and count >= 3:
+                offer_count = math.floor(count / 3)
+                remainder = count % 3
+
+
     else:
         return -1
+
